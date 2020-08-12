aa6f3f89e16631ce71ac9e8a517283dc5aa6453c
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 00:52:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,10 +9,10 @@ def my_hash
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {:name =>'Grace Hopper'}
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  {id:number}
 end
